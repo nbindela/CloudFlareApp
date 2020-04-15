5a226f46b7f42907521874db735167d992b28b67
Nicholas Bindela <njb013@bucknell.edu>
2020-04-15 19:36:06 -0400

    Testing hostname

--- ping.c
+++ ping.c
@@ -2,7 +2,21 @@
 Nicholas Bindela
 CloudFlare Software Engineering Application
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char * argv){
+int main(int argc, char **argv){
+
+  struct hostent *host_name;
+
+  if(argc < 2){
+    perror("Incorrect number of arguments\nUsage: ./ping [hostname or IP]")
+    return 1;
+  }
+  if((host_name = gethostbyname(argv)) != NULL){
+    printf("Hostname is %s\n",*host_name->h_name);
+  }
+
   return 0;
 }
